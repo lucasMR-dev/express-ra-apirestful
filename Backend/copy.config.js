e222d340c951ebb1614c13dbb640b56f3060285c
Lucas Mardones <l.mardones91@gmail.com>
2022-04-11 10:12:35 -0400

    add mongo-init instance instead of using test db by default, fixed example.config

--- Backend/copy.config.js
+++ Backend/copy.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 5000,
-    URL: process.env.BASE_URL || 'http://localhost:5000/v1',
+    URL: process.env.BASE_URL || 'http://0.0.0.0:5000/v1',
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://<user>:<password>@mongo:27017/<db>',
     JWT_SECRET: process.env.JWT_SECRET || 'secret_token',
     // Deployment
-    DEPLOY_URL: process.env.URL || 'http://localhost:5000/v1',
+    DEPLOY_URL: process.env.URL || 'http://0.0.0.0:5000/v1',
     // Jest
     JEST_USER: 'your_user',
     JEST_PASS: 'your_pass'
